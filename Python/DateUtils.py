358b0f418c9a3e3470ad2b49fc51c662fb2e4acf
darkside <34475779+darkside-e@users.noreply.github.com>
2020-06-01 00:28:10 +0300

    update

--- Python/DateUtils.py
+++ Python/DateUtils.py
@@ -2,7 +2,24 @@ from datetime import datetime
 
 
 class DateUtils:
+    DATE_FORMAT = '%d-%m-%Y %I:%M:%S %p'
+
     @staticmethod
     def now():
         now = datetime.now()
-        return now.strftime('%d-%m-%Y %I:%M:%S %p')
+        return now.strftime(DateUtils.DATE_FORMAT)
+
+    @staticmethod
+    def compare_dates(date_a: str, date_b: str):
+        date_a = DateUtils.get_date(date_a)
+        date_b = DateUtils.get_date(date_b)
+        print(date_a, date_b, f'date_a < date_b = {date_a < date_b}', sep=' / ')
+        return date_a < date_b
+
+    # To sort string dates use:
+    # -------------------------
+    # * Ascending Order --> list.sort(key=lambda item: DateUtils.get_date(item['date']))
+    # * Add `reversed=True` for Descending Order
+    @staticmethod
+    def get_date(date: str):
+        return datetime.strptime(date, DateUtils.DATE_FORMAT)
---
+++ Python/Scrapping/chrome.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import time
+
+
+class ChromeManager:
+    _driver = None
+
+    def __init__(self, driver_path='chromedriver', headless=False, window_size='1200x600'):
+        _options = webdriver.ChromeOptions()
+        _options.add_argument(f'window-size={window_size}')
+        if headless:
+            _options.add_argument('headless')
+        self._driver = webdriver.Chrome(driver_path, options=_options)
+        print('[INFO] Driver loaded...')
+
+    def _wait_for_element(self, element_value, element_type, timeout):
+        try:
+            element_present = EC.presence_of_element_located((element_type, element_value))
+            WebDriverWait(self._driver, timeout).until(element_present)
+            print('[INFO] Page loaded')
+            return True
+
+        except TimeoutException:
+            print('[INFO] Timed out waiting for page to load')
+            return False
+
+    def load_page(self, url, wait_element_value, wait_element_type=By.CSS_SELECTOR, wait_timeout=10):
+        self._driver.get(url)
+        return self._wait_for_element(wait_element_value, wait_element_type, wait_timeout)
+
+    def get_elements(self, selector):
+        return self._driver.find_elements_by_css_selector(selector)
+
+    def click_element(self, selector, sleep_after_click=2):
+        try:
+            self._driver.find_element_by_css_selector(selector).click()
+            print('[INFO] Click...')
+            return 1
+
+        except Exception as e:
+            print(f'[ERROR]: {e}')
+            return 0
+
+        finally:
+            if sleep_after_click:
+                time.sleep(sleep_after_click)

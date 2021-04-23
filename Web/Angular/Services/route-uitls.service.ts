b8e41bab855f3c95c8e0e11de0eb4dd35c09e552
ali harakeh <34475779+aliharakeh@users.noreply.github.com>
2021-04-24 00:30:28 +0300

    update

--- Web/Angular/Services/route-uitls.service.ts
+++ Web/Angular/Services/route-uitls.service.ts
@@ -9,8 +9,22 @@ export class RouteUitlsService {
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
 
-  getRouteParams(): Observable<ParamMap> {
+  routeParams(): Observable<ParamMap> {
     return this.activatedRoute.paramMap;
+  }
+  
+  routeData(): Observable<Data> {
+    return this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map(route => {
+        while (route.firstChild) {
+          route = route.firstChild;
+        }
+        return route;
+      }),
+      mergeMap(route => route.data)
+    );
   }
 
   redirectTo(route: string, ...params: any[]): Promise<boolean> {
